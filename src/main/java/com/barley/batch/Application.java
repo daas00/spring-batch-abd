c970f02f5fba47ab896439a346b5a5b1821c782b
KHRIFOU SAAD <saad.khrifou@gmail.com>
2023-07-26 22:17:20 +0100

    Update Application.java

--- src/main/java/com/barley/batch/Application.java
+++ src/main/java/com/barley/batch/Application.java
@@ -3,6 +3,7 @@ package com.barley.batch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Point d'entrée de l'application 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) throws Exception {
